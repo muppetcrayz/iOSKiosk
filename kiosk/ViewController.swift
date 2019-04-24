3fcb58a11beec6a2610e973154c21eeb8583e69c
Sage Conger <sconger@capital.edu>
2019-04-24 16:53:21 -0400

    Initial Commit

---
+++ kiosk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kiosk
+//
+//  Created by Sage Conger on 4/24/19.
+//  Copyright © 2019 dubtel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
